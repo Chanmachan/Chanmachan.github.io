3275b54bb859f035943ea552853ee8161918787a
chanma <yunosuke.homma@icloud.com>
2024-07-01 19:14:00 +0900

    [update]

--- src/home/Works.tsx
+++ src/home/Works.tsx
@@ -66,7 +66,7 @@ const Works = () => {
         <Project
           title='GoChat'
           language='バックエンド-> Go, フロントエンド-> React(ts)'
-          description='OAuth(googleのみ)によるログインのついたリアルタイムチャットアプリ'
+          description='OAuth(googleのみ)によるログインのついたリアルタイムチャットアプリを開発した'
           githubUrl='https://github.com/Chanmachan/GoChat'
         />
 

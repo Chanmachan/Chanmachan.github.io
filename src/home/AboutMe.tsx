f16c10ebd7161cf530bd1efd1af1db918bf4cc4a
chanma <yunosuke.homma@icloud.com>
2024-07-03 04:14:36 +0900

    [update]来歴を追加

--- src/home/AboutMe.tsx
+++ src/home/AboutMe.tsx
@@ -7,6 +7,40 @@ const AboutMe = () => {
     <Box p={6} m={4}>
       <Heading fontSize='2xl' mb={3}>About Me</Heading>
 
+      <Heading fontSize='lg' mt={4} mb={2}>来歴</Heading>
+      <List spacing={2}>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          2003年生まれ
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          開智未来高等学校卒業（令和3年3月）
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          明治大学国際日本学部入学（令和3年4月）
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          42Tokyo 受験・合格（令和4年4月）
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          プログラミングに専念するため大学を休学（令和4年5月）
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          42Tokyo 入学（令和4年7月）
+        </ListItem>
+        <ListItem>
+          <ListIcon as={MdCheckCircle} color='blue.500' />
+          大学に復学しながら42Tokyoで活動中（令和5年8月）
+        </ListItem>
+      </List>
+
+      <Heading fontSize='lg' mt={4} mb={2}>趣味など</Heading>
+
       <List spacing={2}>
         <ListItem>
           <ListIcon as={MdCheckCircle} color='green.500' />
